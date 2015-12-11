8a9cd57dd24e1fd904b84388b750a6fca9a88d92
Joel Jensen <joel.jensen@experticity.com>
2015-12-11 14:52:09 -0700

    Clean up and simplify everything

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-javac -classpath "./jogamp-all-platforms/jar/gluegen-rt.jar:./jogamp-all-platforms/jar/jogl-all.jar" src/Main.java src/Gfx.java
+javac -classpath "./jogamp-all-platforms/jar/gluegen-rt.jar:./jogamp-all-platforms/jar/jogl-all.jar" src/Main.java
 exit $?
 
--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,44 @@
-
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
 import java.util.Random;
+import com.jogamp.opengl.*;
+import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.util.*;
 
-public class Main {
-	public static void main(String args[]) {
-		Gfx.init(new Scene());
-	}
-}
-
-class Scene implements Scenery {
+public class Main implements GLEventListener, KeyListener {
 
 	double x = 0.0;
 	double y = 0.0;
 
 	boolean[] keys = new boolean[256];
 
-	public void draw() {
+	public static void main(String args[]) {
+		GLProfile.initSingleton();
+		GLProfile glp = GLProfile.get(GLProfile.GL2);
+		GLCapabilities caps = new GLCapabilities(glp);
+		GLCanvas canvas = new GLCanvas(caps);
 
-		double speed = 0.01;
+		Frame frame = new Frame("Herp a derp");
+		frame.setSize(1024, 768);
+		frame.add(canvas);
+		frame.setVisible(true);
 
-		if(keys[KeyEvent.VK_DOWN]) {
-			y -= speed;
-		}
-		if(keys[KeyEvent.VK_UP]) {
-			y += speed;
-		}
-		if(keys[KeyEvent.VK_LEFT]) {
-			x -= speed;
-		}
-		if(keys[KeyEvent.VK_RIGHT]) {
-			x += speed;
-		}
+        Main mainInstance = new Main();
 
-		Gfx.dot(x, y, 1, 1, 1);
+		frame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
 
-		Random r = new Random(1234);
+		canvas.addGLEventListener(mainInstance);
+		frame.addKeyListener(mainInstance);
 
-		for(int i = 0; i < 100; i++) {
-			Gfx.dot(r.nextDouble(), r.nextDouble(), 1, 1, 1);
-		}
-
+        FPSAnimator animator = new FPSAnimator(canvas, 60);
+        animator.start();
 	}
 
 	public void keyPressed(KeyEvent e) {
@@ -57,4 +56,44 @@ class Scene implements Scenery {
 	public void keyTyped(KeyEvent e) {
 	}
 
+	public void init(GLAutoDrawable drawable) {
+		drawable.getGL().setSwapInterval(1);
+	}
+
+	public void display(GLAutoDrawable drawable) {
+
+        double speed = 0.01;
+
+        if(keys[KeyEvent.VK_DOWN]) {
+        	y -= speed;
+        }
+        if(keys[KeyEvent.VK_UP]) {
+        	y += speed;
+        }
+        if(keys[KeyEvent.VK_LEFT]) {
+        	x -= speed;
+        }
+        if(keys[KeyEvent.VK_RIGHT]) {
+        	x += speed;
+        }
+
+
+        GL2 gl = drawable.getGL().getGL2();
+
+        gl.glClear(GL.GL_COLOR_BUFFER_BIT);
+
+        gl.glColor3f(1, 1, 1);
+        gl.glBegin(gl.GL_POINTS);
+        gl.glVertex3d(x, y, 0.0);
+        gl.glEnd();
+
+	}
+
+	public void dispose(GLAutoDrawable drawable) {
+	}
+
+	public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+	}
+
 }
+
